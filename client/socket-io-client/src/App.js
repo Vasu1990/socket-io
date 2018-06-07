46a7046372a7fb04b7f1d62bbce2c0f9ad230455
Vasu Nagpal <vnagpal2@sapient.com>
2018-06-07 11:34:08 +0530

    bot code ai

--- client/socket-io-client/src/App.js
+++ client/socket-io-client/src/App.js
@@ -1,31 +1,71 @@
 import React, { Component } from "react";
 import socketIOClient from "socket.io-client";
+import { request } from "https";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       response: false,
-      endpoint: "http://127.0.0.1:4001"
+      endpoint: "http://127.0.0.1:4001",
+      request: ""
     };
+    this.recognition = {}
+    this.notTalking = true;
   }
 
   componentDidMount() {
     const { endpoint } = this.state;
     const socket = socketIOClient(endpoint);
-    socket.on("FromAPI", data => this.setState({ response: data }));
+
+    const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    this.recognition = new SpeechRecognition();
+
+
+    this.recognition.addEventListener('result', e => {
+      const last = e.results.length - 1;
+      const text = e.results[last][0].transcript;
+    
+      console.log('result: ' + e.results[0][0].confidence);
+      console.log('text: ' + text);
+      this.setState({request : text} , () => {
+        socket.emit("chatMessage", text);
+      });
+    });
+
+
+    this.recognition.onend =  () => {
+      this.notTalking = true;
+    };
+
+    this.recognition.onerror = () => {
+      this.notTalking = true;
+    };
+
+    socket.on('bot reply', data => this.setState({ response: data })); 
+
   }
 
+  talkNow = () => {
+    if(this.notTalking) {
+      this.notTalking = false;
+      this.recognition.start();
+    }
+  }
+  
+
   render() {
-    const { response } = this.state;
+    const { response,request } = this.state;
     return (
-      <div style={{ textAlign: "center" }}>
-        {response
-          ? <p>
-              The temperature in Florence is: {response} °F
-            </p>
-          : <p>Loading...</p>}
-      </div>
+        <div style={{ textAlign: "center" }}>
+            <button onClick={this.talkNow}>talk</button>
+            <div>
+              Request: {request}
+            </div>
+            <div>
+              Response: {response}
+            </div>
+        </div>
     );
   }
 }
